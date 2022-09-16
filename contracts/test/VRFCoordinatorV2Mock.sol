04119926ae268008d7aaeb62c3bcbc15fbcd938b
saumyanayak <saumyanayak264@gmail.com>
2022-09-17 04:46:09 +0530

    pushing

--- contracts/test/VRFCoordinatorV2Mock.sol
+++ contracts/test/VRFCoordinatorV2Mock.sol
@@ -1,8 +1,4 @@
-//SPDX-License-Identifier:MIT
-
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
-
-//Here we have added the mock VRF contract from chainlink so that we can deploy this contract
-//on our local network for our Lottery contract to use it in our local hardhat environment
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,3 @@
-// require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-etherscan")
 require("hardhat-deploy")
@@ -7,51 +6,104 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
+    "https://eth-goerli.alchemyapi.io/v2/phqhjHMPaSls8fNGkNIeVn1nIyQKkVfs"
+const POLYGON_MAINNET_RPC_URL =
+    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+// optional
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+const REPORT_GAS = process.env.REPORT_GAS || false
 
 module.exports = {
-  solidity: "0.8.9",
-  defaultNetwork: "hardhat",
-  networks:{
-    hardhat: {
-      chainId : 31337,
-      blockConfirmations : 1,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            // // If you want to do some forking, uncomment this
+            // forking: {
+            //   url: MAINNET_RPC_URL
+            // }
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 5,
+        },
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 1,
+        },
+        polygon: {
+            url: POLYGON_MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 137,
+        },
     },
-    rinkeby:{                         //staging tests will run on rinkeby 
-      url : RINKEBY_RPC_URL,
-      account : [PRIVATE_KEY],
-      chainId : 4,
-      blockConfirmations : 3
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+            polygon: POLYGONSCAN_API_KEY,
+        },
     },
-    goerli:{
-      url: "https://goerli.infura.io/v3/",
-      account : [PRIVATE_KEY],
-      chainId : 5,
-      blockConfirmations : 3,
-    }
-  },
-  namedAccounts : {
-    deployer :{
-      default : 0
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
-    player : {
-      default : 1
-    }
-  },
-  gasReporter: {
-    enabled: false,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "ETH",
-  },
-  etherscan: {
-      apiKey: ETHERSCAN_API_KEY,
-  },
-};
+    contractSizer: {
+        runOnCompile: false,
+        only: ["Raffle"],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
+    mocha: {
+        timeout: 500000, // 500 seconds max for running tests
+    },
+}
